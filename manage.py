fc6677cb07093a32cddcf3e21360b97162aabe8d
Albert Ashaba Aheebwa <trippleaunit@hotmail.com>
2023-12-19 23:17:20 +0300

    Separate settings module

--- manage.py
+++ manage.py
@@ -5,8 +5,18 @@ import sys
 
 
 def main():
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    env = os.environ.get("ENV")
+
+    if env and env == "dev":
+        settings_module = "musik.local_settings"
+    else:
+        settings_module = "musik.settings"
+
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musik.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +28,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
