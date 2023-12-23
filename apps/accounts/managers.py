c368eb02a65025ee13d494cadf2e502e8cc39177
Albert Ashaba Aheebwa <trippleaunit@hotmail.com>
2023-12-23 08:52:07 +0300

    refactor managers

---
+++ apps/accounts/managers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import BaseUserManager, Group
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        if password is None or not password.strip():
+            raise ValueError("The Password field must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        if not user.groups.filter(name="user").exists():
+            user.groups.add(Group.objects.get(name="user"))
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
